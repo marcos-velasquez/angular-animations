4b7bf6c0f27eb8068ce1718971376d7f503442df
Marcos Velasquez <marcos@socialgest.net>
2025-10-12 23:29:34 -0400

    Refactor directive timeline handling to use computed properties for improved performance and consistency

--- src/app/directives/_gsap-host.directive.ts
+++ src/app/directives/_gsap-host.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnDestroy, OnInit, effect, input, output, signal } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit, computed, effect, input, output, signal } from '@angular/core';
 import { gsap } from 'gsap';
 import { Trigger, TriggerRef, TriggerType, GsapHostTweenVarsBuilder } from './models/_index';
 
@@ -14,12 +14,14 @@ export abstract class GsapHostDirective implements OnInit, OnDestroy {
   public readonly animateComplete = output<GsapHostDirective>();
 
   public readonly triggerRef = signal<TriggerRef>(Trigger.empty());
-  protected readonly timeline = gsap.timeline({ paused: true, defaults: new GsapHostTweenVarsBuilder(this).build() });
+  protected readonly timeline = computed(() =>
+    gsap.timeline({ paused: true, defaults: new GsapHostTweenVarsBuilder(this).build() })
+  );
 
   constructor(protected readonly el: ElementRef<HTMLElement>) {
     effect(() => this.animate());
-    this.timeline.eventCallback('onStart', () => this.animateStart.emit(this));
-    this.timeline.eventCallback('onComplete', () => this.animateComplete.emit(this));
+    this.timeline().eventCallback('onStart', () => this.animateStart.emit(this));
+    this.timeline().eventCallback('onComplete', () => this.animateComplete.emit(this));
   }
 
   ngOnInit(): void {
@@ -28,24 +30,24 @@ export abstract class GsapHostDirective implements OnInit, OnDestroy {
 
   public abstract animate(): void;
 
-  public pause(): void {
-    this.timeline.pause();
+  public play(): void {
+    this.timeline().play(0);
   }
 
-  public play(): void {
-    this.timeline.play(0);
+  public pause(): void {
+    this.timeline().pause();
   }
 
   public reverse(): void {
-    this.timeline.reverse();
+    this.timeline().reverse();
   }
 
   public resume(): void {
-    this.timeline.resume();
+    this.timeline().resume();
   }
 
   public restart(): void {
-    this.timeline.restart();
+    this.timeline().restart();
   }
 
   ngOnDestroy(): void {
--- src/app/directives/rotate.directive.ts
+++ src/app/directives/rotate.directive.ts
@@ -6,6 +6,6 @@ export class RotateDirective extends GsapHostDirective {
   public readonly rotate = input.required<number>();
 
   public animate() {
-    this.timeline.from(this.el.nativeElement, { rotate: this.rotate() });
+    this.timeline().from(this.el.nativeElement, { rotate: this.rotate() });
   }
 }
