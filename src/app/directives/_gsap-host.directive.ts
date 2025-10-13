280cb10d3a12b11db2cb0fe97a84743e339e4f83
Marcos Velasquez <marcos@socialgest.net>
2025-10-12 22:44:56 -0400

    Refactor GsapHostDirective to enhance animation handling and trigger management

--- src/app/directives/_gsap-host.directive.ts
+++ src/app/directives/_gsap-host.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnDestroy, OnInit, input, signal } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit, effect, input, output, signal } from '@angular/core';
 import { gsap } from 'gsap';
 import { Trigger, TriggerRef, TriggerType } from './models/trigger';
 
@@ -10,13 +10,20 @@ export abstract class GsapHostDirective implements OnInit, OnDestroy {
   public readonly ease = input<gsap.EaseString>('power1.out');
   public readonly trigger = input<TriggerType>(Trigger.default);
 
-  protected readonly triggerRef = signal<TriggerRef>(Trigger.empty());
-  protected readonly timeline = gsap.timeline();
+  public readonly animateStart = output<GsapHostDirective>();
+  public readonly animateComplete = output<GsapHostDirective>();
 
-  constructor(protected readonly el: ElementRef<HTMLElement>) {}
+  protected readonly triggerRef = signal<TriggerRef>(Trigger.empty());
+  protected readonly timeline = gsap.timeline({ paused: true });
+
+  constructor(protected readonly el: ElementRef<HTMLElement>) {
+    effect(() => this.animate());
+    this.timeline.eventCallback('onStart', () => this.animateStart.emit(this));
+    this.timeline.eventCallback('onComplete', () => this.animateComplete.emit(this));
+  }
 
   ngOnInit(): void {
-    this.triggerRef.set(new Trigger(this.el.nativeElement).when(this.trigger()).then(this.animate.bind(this)));
+    this.triggerRef.set(new Trigger(this.el.nativeElement).when(this.trigger()).then(this.play.bind(this)));
   }
 
   public abstract animate(): void;
@@ -26,11 +33,19 @@ export abstract class GsapHostDirective implements OnInit, OnDestroy {
   }
 
   public play(): void {
-    this.timeline.play();
+    this.timeline.play(0);
   }
 
   public reverse(): void {
     this.timeline.reverse();
+  }
+
+  public resume(): void {
+    this.timeline.resume();
+  }
+
+  public restart(): void {
+    this.timeline.restart();
   }
 
   ngOnDestroy(): void {
