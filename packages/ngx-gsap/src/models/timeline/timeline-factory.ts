7db74d3921deb511278a559b85e5ac7861680fac
Marcos Velasquez <marcos@socialgest.net>
2025-10-23 20:21:10 -0400

    fix: use type-safe assertions in TimelineFactory for null values

--- packages/ngx-gsap/src/models/timeline/timeline-factory.ts
+++ packages/ngx-gsap/src/models/timeline/timeline-factory.ts
@@ -16,10 +16,10 @@ export class TimelineFactory {
   }
 
   public create(): Timeline {
-    return cache.get(this.host)!;
+    return cache.get(this.host) as Timeline;
   }
 
   public static empty(): Timeline {
-    return new Timeline(null as any, {});
+    return new Timeline(null as unknown as HTMLElement, {});
   }
 }
